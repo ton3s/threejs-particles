3478700dccd503b1223364c8bcaed9fc87b9171a
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-02-06 00:03:24 -0600

    Custom geometry

--- src/script.js
+++ src/script.js
@@ -24,13 +24,21 @@ const textureLoader = new THREE.TextureLoader()
  */
 
 // Geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 5000
+const positions = new Float32Array(count * 3)
+for (let i = 0; i < count * 3; i++) {
+	positions[i] = (Math.random() - 0.5) * 10
+}
+particlesGeometry.setAttribute(
+	'position',
+	new THREE.BufferAttribute(positions, 3)
+)
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-	size: 0.02,
+	size: 0.1,
 	sizeAttenuation: true,
-	color: '#ff0000',
 })
 
 // Points
